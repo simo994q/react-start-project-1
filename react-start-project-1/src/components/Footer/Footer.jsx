bda58ebe9a932424ea708fb88ed3e11a2f9cb9d0
simo994q <simonkn1@hotmail.com>
2023-08-16 11:28:10 +0200

    formvalidering

--- react-start-project-1/src/components/Footer/Footer.jsx
+++ react-start-project-1/src/components/Footer/Footer.jsx
@@ -4,6 +4,31 @@ import footerBgColor from '../../images/Bg-Tile-Blue-Light.png'
 import footerLogo from '../../images/KT-Logo.png'
 
 export function Footer() {
+    function checkValid(event) {
+        event.preventDefault()
+        let errorMessage = 'Du mangler at udfylde:\n'
+
+        if (event.target.personName.value.length == 0) {
+            errorMessage = errorMessage + '\nNavn'
+        }
+
+        if (event.target.personCity.value.length == 0) {
+            errorMessage = errorMessage + '\nBy'
+        }
+
+        if (event.target.personEmail.value.length == 0) {
+            errorMessage = errorMessage + '\nEmail'
+        }
+
+        if (event.target.personPhone.value.length == 0) {
+            errorMessage = errorMessage + '\nTelefon'
+        }
+
+        if (event.target.personName.value.length == 0 || event.target.personCity.value.length == 0 || event.target.personEmail.value.length == 0 || event.target.personPhone.value.length == 0) {
+            alert(errorMessage)
+        }
+
+    };
     return (
         <div className={style.footerContainer} style={{ backgroundImage: `url(${footerBgColor})` }}>
             <section className={style.footerAbout}>
@@ -23,20 +48,22 @@ export function Footer() {
             </section>
 
             <section className={style.footerForm}>
-                <h3>Ja tak! Jeg vil gerne høre mere</h3>
-                <input type="text" placeholder='Fulde navn' />
-                <input type="text" placeholder='Hjemby' />
-                <input type="email" placeholder='Email' />
-                <input type="number" placeholder='Telefon' />
-                <div>
-                    <p>Få en samtale med Klimatossen</p>
-                    <input type="checkbox" />
-                </div>
-                <div>
-                    <p>Modtag nyhedsbrev</p>
-                    <input type="checkbox" />
-                </div>
-                <button>Send</button>
+                <form onSubmit={(event) => checkValid(event)}>
+                    <h3>Ja tak! Jeg vil gerne høre mere</h3>
+                    <input type="text" placeholder='Fulde navn' id='personName' name='personName'/>
+                    <input type="text" placeholder='Hjemby' id='personCity' name='personCity'/>
+                    <input type="email" placeholder='Email' id='personEmail' name='personEmail'/>
+                    <input type="number" placeholder='Telefon' id='personPhone' name='personPhone'/>
+                    <div>
+                        <p>Få en samtale med Klimatossen</p>
+                        <input type="checkbox" />
+                    </div>
+                    <div>
+                        <p>Modtag nyhedsbrev</p>
+                        <input type="checkbox" />
+                    </div>
+                    <input type='submit' id='formButton' value='send'></input>
+                </form>
             </section>
         </div>
     )
